de956d717be4c44a2017ed50f3e02b49825ee03a
saasbook <saasbook@saasbook.(none)>
2015-11-13 10:02:14 -0400

    1 teste de rspec

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -25,6 +25,7 @@ class Admin::CategoriesController < Admin::BaseController
 
   def new_or_edit
     @categories = Category.find(:all)
+    #@category = Category.find(params[:id])
     @category = params[:id] ? Category.find(params[:id]) : Category.new
     @category.attributes = params[:category]
     if request.post?
--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -15,7 +15,18 @@ describe Admin::CategoriesController do
     get :index
     assert_response :redirect, :action => 'index'
   end
-
+  
+  describe "test_new"  do
+    before(:each) do
+      get :new
+    end
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+  end
+  
   describe "test_edit" do
     before(:each) do
       get :edit, :id => Factory(:category).id
@@ -38,7 +49,7 @@ describe Admin::CategoriesController do
     post :edit, :id => Factory(:category).id
     assert_response :redirect, :action => 'index'
   end
-#.
+
   describe "test_destroy with GET" do
     before(:each) do
       test_id = Factory(:category).id
